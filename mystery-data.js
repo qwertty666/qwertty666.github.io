3673772cb6fe0e416768f633c3b47bd5f3d59838
火云邪神 <1987681736@qq.com>
2026-08-17 00:45:39 +0800

    Create mystery-data.js

---
+++ mystery-data.js
@@ -0,0 +1,54 @@
+// mystery-data.js
+// 神秘图片 - 所有图片数据
+window.mysteryData = {
+    // ===== 页面标题和副标题 =====
+    title: '神秘图片',
+    subtitle: '温馨提示：境外托管，请耐心等待图片加载。最多2分钟。建议将网址复制到浏览器打开。如图片长时间未加载出，请刷新页面。',
+
+    // ===== 图片列表（每个卡片） =====
+    images: [
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_1063_esqw8p',
+            caption: '牢班睡觉中………'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_1059_zcy96m',
+            caption: '沉思中的菲巴睿'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_1068_rs0lvd',
+            caption: '恶作剧。'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_0128_akpzm3',
+            caption: 'XXL扣bs'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_1070_apxfgb',
+            caption: '大事迹'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/47AD5F440B5E9A2C8585E4B4AE09DD62_asn4s0',
+            caption: '班长依旧睡觉。'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_1071_hcibbd',
+            caption: '王茂钢的怪物几把。'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_0615_o2oxpm',
+            caption: '肛大头。'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_0767_w82bng',
+            caption: '肛之恋。'
+        },
+        {
+            src: 'https://res.cloudinary.com/dc6yzsom9/image/upload/q_auto,f_auto/IMG_0334_m5quxs',
+            caption: '肛毛的夜店荣耀。'
+        }
+    ],
+
+    // ===== 页脚版权 =====
+    footer: '© 2026 HELL OF CDFZ'
+};
