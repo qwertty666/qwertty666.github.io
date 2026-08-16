4ab4ab481bcf599c586c2e884e2fe2b4d01eaa9c
火云邪神 <1987681736@qq.com>
2026-08-16 18:47:50 +0800

    Delete auth.js

--- auth.js
+++
@@ -1,7 +0,0 @@
-// 最先运行，页面还没有渲染，没有机会看见内容
-(function() {
-    const AUTH_KEY = 'site_authenticated';
-    if (sessionStorage.getItem(AUTH_KEY) !== 'true') {
-        window.location.href = '/login.html';
-    }
-})();
