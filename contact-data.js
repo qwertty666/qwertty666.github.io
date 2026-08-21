61ca1b5df4436abbc79e39a68c68cf7a72ffe5bc
火云邪神 <1987681736@qq.com>
2026-08-22 04:15:08 +0800

    Update contact-data.js

--- contact-data.js
+++ contact-data.js
@@ -42,8 +42,8 @@ window.contactData = {
 
     // ===== 彩蛋链接 =====
     easterEgg: {
-        text: '点击！！！跳转至地狱网特供VPN节点，密码为王茂钢sfz号(禁止滥用)',
-        url: 'https://lesile.de5.net/admin'
+        text: '小彩蛋',
+        url: 'https://res.cloudinary.com/dc6yzsom9/image/upload/BA2202FD-231F-44FB-A1C2-9A476E3C1700_ilialx'
     },
 
     // ===== 页脚版权 =====
